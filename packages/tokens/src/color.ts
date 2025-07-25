9196d8c65133f74a4180545fc947eb0b971aeec5
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-07-26 08:31:31 +0900

    feat: add tokens package

---
+++ packages/tokens/src/color.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  // gray
+  gray100: '#F5F5F5',
+  gray200: '#E7E7E7',
+  gray300: '#BBBABA',
+  gray400: '#929292',
+  gray500: '#787777',
+  gray600: '#565656',
+  gray700: '#403F3F',
+  gray800: '#262424',
+  gray900: '#1B1818',
+} as const;

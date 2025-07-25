c36e13593fc133b067c14d761480744a5d9522e4
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-07-26 08:35:20 +0900

    feat: update tailwind and tsconfig for monorepo structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
-import { typography, colors } from '@/tokens';
+import { colors, typography } from '@redot/tokens';
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './.storybook/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './packages/ui/src/**/*.{js,ts,jsx,tsx}',
+    './.storybook/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors,

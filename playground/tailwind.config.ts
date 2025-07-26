a8e08ad039a1e11ec0729e78ea9e475c1227dcb3
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-07-26 09:25:00 +0900

    feat: initialize playground based on React + Vite

---
+++ playground/tailwind.config.ts
@@ -0,0 +1,14 @@
+import { colors, typography } from '@redot/tokens';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors,
+      fontSize: typography.size,
+      fontWeight: typography.weight,
+    },
+  },
+  plugins: [],
+} satisfies Config;

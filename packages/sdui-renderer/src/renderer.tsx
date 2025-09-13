e6506e7199f2ae0d6abe4c959fbf5afdd8215c75
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-09-14 01:34:00 +0900

    fix: add id props in SDUIRenderer component

--- packages/sdui-renderer/src/renderer.tsx
+++ packages/sdui-renderer/src/renderer.tsx
@@ -5,15 +5,19 @@ function SDUIRenderer({ content }: { content: ContentBlock }) {
   const Comp = COMPONENT_MAP[content.component] || content.component;
 
   if (!content.children) {
-    return <Comp {...content.props} />;
+    return <Comp id={content.id} {...content.props} />;
   }
 
   if (typeof content.children === 'string') {
-    return <Comp {...content.props}>{content.children}</Comp>;
+    return (
+      <Comp id={content.id} {...content.props}>
+        {content.children}
+      </Comp>
+    );
   }
 
   return (
-    <Comp {...content.props}>
+    <Comp id={content.id} {...content.props}>
       {content.children.map((child: ContentBlock) => (
         <SDUIRenderer key={child.id} content={child} />
       ))}

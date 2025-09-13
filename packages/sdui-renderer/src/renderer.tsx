1f4dd172271136217fad273aa8879001e0635efb
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-09-14 01:52:27 +0900

    fix: remove unnecessary id prop from SDUIRenderer component

--- packages/sdui-renderer/src/renderer.tsx
+++ packages/sdui-renderer/src/renderer.tsx
@@ -5,19 +5,15 @@ function SDUIRenderer({ content }: { content: ContentBlock }) {
   const Comp = COMPONENT_MAP[content.component] || content.component;
 
   if (!content.children) {
-    return <Comp id={content.id} {...content.props} />;
+    return <Comp {...content.props} />;
   }
 
   if (typeof content.children === 'string') {
-    return (
-      <Comp id={content.id} {...content.props}>
-        {content.children}
-      </Comp>
-    );
+    return <Comp {...content.props}>{content.children}</Comp>;
   }
 
   return (
-    <Comp id={content.id} {...content.props}>
+    <Comp {...content.props}>
       {content.children.map((child: ContentBlock) => (
         <SDUIRenderer key={child.id} content={child} />
       ))}

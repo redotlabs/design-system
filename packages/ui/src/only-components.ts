e3a3cdee516969b640c41783a22cd0e1cd0ee937
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-11-08 22:17:08 +0900

    feat(ui): add table components and exports

--- packages/ui/src/only-components.ts
+++ packages/ui/src/only-components.ts
@@ -1,4 +1,21 @@
 export { Badge } from './badge';
 export { Button } from './button';
+export { Checkbox } from './checkbox';
 export { Input } from './input';
-export { Select } from './select';
+export { Radio } from './radio';
+export {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from './select';
+export {
+  Table,
+  TableHeader,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableFooter,
+} from './table';
